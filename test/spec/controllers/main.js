19b064abbff4c6f106bbd5af05b6d8b5186ae16a
Joe Ferrie <jferrie@cdl-jferrie-9m.ad.ucop.edu>
2014-10-06 09:56:13 -0700

    Forced tests to pass

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('cdlaUiApp'));
+  beforeEach(module('cdlaControllers'));
 
   var MainCtrl,
     scope;
