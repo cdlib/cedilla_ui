6f758f5c3123bc58c52dd44c0927b32cb570c79c
jferrie <joe.ferrie@ucop.edu>
2014-11-13 14:36:44 -0800

    new release image

--- Gruntfile.js
+++ Gruntfile.js
@@ -66,7 +66,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9000,
+        port: 18880,
         // Change this to 'localhost' to prevent access to the server from outside.
         hostname: '0.0.0.0',
         livereload: 35729
