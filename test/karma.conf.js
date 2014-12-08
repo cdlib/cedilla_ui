670a5f5dd02e1992ac9922eb66fa94fb7eb09157
jferrie <joe.ferrie@ucop.edu>
2014-12-08 13:01:41 -0800

    using file for test socket.io client

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'http://cdla-api-stg.cdlib.org:3005/socket.io/socket.io.js',
+      'test/lib/socket.io.js',
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/lodash/dist/lodash.compat.js',
